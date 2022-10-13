3c8f2b3e7d8fc08fd1d43d8b1464cb25a1b3930a
iamEmmaWang <115750171+iamEmmaWang@users.noreply.github.com>
2022-10-13 16:59:11 -0700

    created Song player.dart
    This page will be where audio is played given the mode, and where the music notes page will be shown

---
+++ lib/Pages/SongPlayer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SongPlayer extends StatefulWidget {
+  const SongPlayer({Key? key}) : super(key: key);
+
+  @override
+  State<SongPlayer> createState() => _SongPlayerState();
+}
+
+class _SongPlayerState extends State<SongPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
